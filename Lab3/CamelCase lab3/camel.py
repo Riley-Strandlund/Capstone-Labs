1590947429812d990e3e4caedb61b492a26f3243
Riley-Strandlund <FFOrstrandlund@gmail.com>
2024-01-24 16:22:56 -0600

    Add benner welcome message a start of program

--- Lab3/CamelCase lab3/camel.py
+++ Lab3/CamelCase lab3/camel.py
@@ -8,7 +8,14 @@ def camelcase(sentence):
     # So this still works on empty strings, strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """ Display program name""" # Triple quotes are docstrings that can describe chunks of code
+    message = "Awesome camelcase program!!"
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
